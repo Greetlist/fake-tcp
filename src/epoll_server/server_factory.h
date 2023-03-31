8f435a1766c17470d9282e9f055e85f92986080d
Greetlist <454281389@qq.com>
2023-03-31 13:24:40 +0800

    add parallel args

--- src/epoll_server/server_factory.h
+++ src/epoll_server/server_factory.h
@@ -8,17 +8,16 @@ class EpollServerFactory {
  public:
    EpollServerFactory() = delete;
    ~EpollServerFactory();
-   static EpollServerBase* GetServer(const ServerType& server_type, const std::string& listen_addr="0.0.0.0", const int& listen_port=1000) {
+   static EpollServerBase* GetServer(const ServerType& server_type, const std::string& listen_addr="0.0.0.0", const int& listen_port=1000, const int& parallel) {
      EpollServerFactory::InitServer(server_type, listen_addr, listen_port);
      return instance_;
    }
 
-   static void InitServer(const ServerType& server_type, const std::string& listen_addr, const int& listen_port) {
+   static void InitServer(const ServerType& server_type, const std::string& listen_addr, const int& listen_port, const int& parallel) {
      if (instance_ == nullptr) {
        std::call_once(instance_guard_, [=]() {
          if (server_type == ServerType::TCP) {
            EpollRunMode mode = EpollRunMode::UseProcess;
-           int parallel = 5;
            instance_ = new EpollTCPServer(mode, parallel, listen_addr, listen_port);
          } else if (server_type == ServerType::UDP) {
            instance_ = new EpollUDPServer(listen_addr, listen_port);
